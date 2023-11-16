41226b95e62ab97c1e4b037c3199add759bbbdcc
yunier.broche <yunier.broche@cencosudscotiabank.cl>
2023-11-16 16:46:25 -0300

    fix alt images

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import Search from '@/app/ui/search';
-import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
+import Image from "next/image";
+import { lusitana } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CustomersTable, FormattedCustomersTable } from "@/app/lib/definitions";
 
 export default async function CustomersTable({
   customers,
@@ -26,18 +26,7 @@ export default async function CustomersTable({
                   >
                     <div className="flex items-center justify-between border-b pb-4">
                       <div>
-                        <div className="mb-2 flex items-center">
-                          <div className="flex items-center gap-3">
-                            <Image
-                              src={customer.image_url}
-                              className="rounded-full"
-                              alt={`${customer.name}'s profile picture`}
-                              width={28}
-                              height={28}
-                            />
-                            <p>{customer.name}</p>
-                          </div>
-                        </div>
+                        <div className="mb-2 flex items-center"></div>
                         <p className="text-sm text-gray-500">
                           {customer.email}
                         </p>
