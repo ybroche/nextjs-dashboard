ca7515a195f4a053a8a3eabbb0c737e23185285a
yunier.broche <yunier.broche@cencosudscotiabank.cl>
2023-11-25 17:02:45 -0300

    add pagination customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -5,21 +5,35 @@ import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { Metadata } from "next";
-import { fetchFilteredCustomers } from "@/app/lib/data";
+import { fetchCustomersPages, fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default async function Page() {
-  //const query = searchParams?.query || "";
-  //const currentPage = Number(searchParams?.page) || 1;
-  //const totalPages = await fetchCustomersPages(query);
-  const customers = await fetchFilteredCustomers("");
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchCustomersPages(query);
 
   return (
-    <Suspense fallback={<InvoicesTableSkeleton />}>
-      <Table customers={customers} />
-    </Suspense>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
   );
 }
